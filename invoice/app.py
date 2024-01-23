66bb6103c9a1473b01154f5dee59e525955d73de
oldbright22 <125103467+oldbright22@users.noreply.github.com>
2024-01-22 20:33:12 -0800

    Update app.py

--- invoice/app.py
+++ invoice/app.py
@@ -1,14 +1,9 @@
-
 from dotenv import load_dotenv
-load_dotenv()
-
 import streamlit as st 
 import os
 
 from PIL import Image
 import google.generativeai as genai
-
-genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 from langchain.document_loaders import TextLoader
 from langchain.embeddings import OpenAIEmbeddings
@@ -23,31 +18,10 @@ from langchain.chains import RetrievalQA
 #VECTARA_CORPUS_ID = os.getenv('VECTARA_CORPUS_ID')
 #VECTARA_API_KEY = os.getenv('VECTARA_API_KEY')
 
+load_dotenv()
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+
 ## Function to load Gemini Pro Vision
 
 
 
-def langchain_func(file, question):
-    loader = TextLoader(file, encoding='utf8')
-    documents = loader.load()
-    vectara = Vectara.from_documents(documents, embedding=None)
-    qa = RetrievalQA.from_llm(llm=OpenAI(), retriever = vectara.as_retriever())
-    answer = qa({"query": question})
-    return answer
-
-
-# Streamlit Code
-st.title("Langchain + Vectara Demo App")
-
-file_uploader = st.file_uploader("Upload your File", type=['txt'])
-
-if file_uploader is not None:
-    file_name = file_uploader.name
-    with open(os.path.join(file_name),"wb") as f:
-            f.write(file_uploader.getbuffer())
-    question = st.text_area("Enter your Query")
-    if st.button("Retrieve"):
-        if len(question) > 0:
-            answer = langchain_func(file_name, question)
-            st.info("Your Question: "+ question)
-            st.success("Your Answer: "+ answer['result'])
